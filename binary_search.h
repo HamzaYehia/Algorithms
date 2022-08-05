2e32bbd6532aa8f7b5a003e87f67591881b25055
HamzaYehia <hamzayehiamakhlouf@gmail.com>
2022-08-05 14:27:46 +0200

    updated

--- binary_search.h
+++ binary_search.h
@@ -1,3 +1,9 @@
+/**
+ * Broken !!
+ * Read more: https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+ */
+
+
 int binary_search(int* nums, int numsSize, int target) {
     if (nums == NULL) return -1;
 
@@ -9,7 +15,7 @@ int binary_search(int* nums, int numsSize, int target) {
         if (nums[mid] == target) return mid;
 
         if (nums[mid] < target) left = mid + 1;
-        else right = mid - 1;
+        if (nums[mid] > target) right = mid - 1;
     }
     return -1;
 }
